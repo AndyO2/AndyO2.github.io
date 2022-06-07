1231d5824259db2321146fccd3618c8d7aedd54d
AndyO2 <andy.okamoto@gmail.com>
2022-06-07 13:39:15 -0600

    updated nav bar to be buttons with scroll event

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let aboutMeButton = document.querySelector('.aboutMeScrollLink');
+aboutMeButton.addEventListener('click', ()=>{
+    window.scroll({
+        top:1000,
+        behavior: 'smooth'
+    })
+})
+
+let personalProjectsButton = document.querySelector('.personalProjectsScrollLink');
+personalProjectsButton.addEventListener('click', ()=>{
+    window.scroll({
+        top:1300,
+        behavior: 'smooth'
+    })
+})
+
+let contactButton = document.querySelector('.contactScrollLink');
+contactButton.addEventListener('click', ()=>{
+    window.scroll({
+        top:1000,
+        behavior: 'smooth'
+    })
+})
